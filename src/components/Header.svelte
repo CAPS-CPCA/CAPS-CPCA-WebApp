6c1cfa43ee4cdc5481872e457d1ca15d76830009
dxncrv <doncorve@gmail.com>
2024-05-21 13:21:15 -0700

    basic routing and global css setup

---
+++ src/components/Header.svelte
@@ -0,0 +1,46 @@
+<nav>
+    <div class="partner-sites">
+        <ul>
+            <li>Medication Abortion</li>
+            <li>sogc.org</li>
+        </ul>
+    </div>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/prescribing">prescribing</a></li>
+            <li><a href="/dispensing">dispensing</a></li>
+            <li><a href="/supporting">supporting roles</a></li>
+            <li><a href="/faqs">faqs</a></li>
+            <li><a href="/about">about us</a></li>
+            <li><a href="/search">search</a></li>
+        </ul>
+</nav>
+
+<style>
+    * {
+        box-sizing: border-box;
+        border: 1px solid blue;
+    }
+/* Specific Sheet for Navigation bar */
+    .partner-sites {
+        height: 45px;
+        width: 100%;
+        display: flex;
+        justify-content: flex-end;
+        gap: 1rem;
+        background: white;
+    }
+    ul {
+        gap: 1rem;
+        display: flex;
+        list-style-type: none;
+    }
+    ul li a {
+        font-family: 'Roboto', sans-serif;
+        font-size: 1rem;
+        font-weight: 600;
+        text-transform: uppercase;
+        text-decoration: none;
+        color: #333;
+    }
+</style>
