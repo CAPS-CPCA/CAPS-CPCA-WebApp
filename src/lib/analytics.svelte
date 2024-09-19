4511f7dd6a2972c57461e215af7a4ef64e76e815
dxncrv <doncorve@gmail.com>
2024-09-18 18:12:31 -0700

    integration: google analytics

--- src/lib/analytics.svelte
+++ src/lib/analytics.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
 	$: {
 		if (typeof gtag !== 'undefined') {
-			gtag('config', 'G-XXXXXXXXXX', {
+			gtag('config', 'G-5YRPHXLWFC', {
 				page_title: document.title,
 				page_path: $page.url.href
 			});
@@ -11,13 +11,13 @@
 </script>
 
 <svelte:head>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-5YRPHXLWFC"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
 		function gtag() {
 			dataLayer.push(arguments);
 		}
 		gtag('js', new Date());
-		gtag('config', 'G-XXXXXXXXXX');
+		gtag('config', 'G-5YRPHXLWFC');
 	</script>
 </svelte:head>
